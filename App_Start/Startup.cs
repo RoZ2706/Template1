4af429929de6e183ed5ca1c36c15fe1c308606be
RoZ <imblocker276@gmail.com>
2022-05-17 12:29:18 +0700

    realtimeee

---
+++ App_Start/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(Template1.App_Start.Startup))]
+
+namespace Template1.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
---
+++ Scripts/ChatIndex.js
@@ -0,0 +1,22 @@
+﻿var chat = $.connection.chatHub;
+
+chat.client.addNewMessageToChatBox = function (name, message) {
+    console.log("RECEIVED")
+    $(".chat-box").prepend("<div class=\"chat-item left\">" +
+        "<div class=\"avatar\" style=\"background-image: url('../../libs/assets/myavt.jpg');\"></div>" +
+        "<div class=\"content\">" +
+        "<p>" + message + "</p>" +
+        "<strong class=\"limit-1\"> @DateTime.Now.ToString(\"HH:mm dd/MM/yyyy\") - " + name + "</strong>" +
+        "</div>" +
+        "</div>");
+}
+
+$.connection.hub.start().done(function () {
+    $('#sendButton').click(function () {
+        if ($('#chatMessage').val().length > 0) {
+            chat.server.send("sender01", $('#chatMessage').val());
+            $('#chatMessage').val('').focus();
+            console.log("SENT")
+        }
+    });
+});
